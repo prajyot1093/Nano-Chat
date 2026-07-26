d2728134c04b8baa36b66dc73011a75f6180a25c
PRAJYOT PUNDE <prajyot1093@gmail.com>
2026-07-26 21:23:48 +0530

    feat : api created for signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,64 @@
-export const signup = (req,res)=> {
-    res.send("signup route")
+import { generateTokens } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+
+// create async await function to get email pass anf fullname 
+export const signup = async (req, res) => {
+  
+  //this was fetched through that middleware in index.js
+    const { fullName, email, password } = req.body;
+ 
+ // try catch block to check all conditions 
+ 
+    try {
+//checking pass lenght 
+    if (password.lenght < 6) {
+      return res
+        .status(400)
+        .json({ message: "password must me atleast 6 charactors" });
+    }
+//checking if email exists 
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists !" });
+ 
+// hashing pass by bcrypt 
+
+const salt = await bcrypt.genSalt(10)
+//store hashed pass
+const hassedPassword = await bcrypt.hash(password,salt)
+
+
+const NewUser = new User({
+//did not change name and email coz not hashing it 
+    fullName,
+    email,
+    password:hassedPassword,
+})
+
+// New user created in DB
+if(NewUser){
+
+    // generate jwt here 
+    generateTokens(NewUser._id,res)
+    await NewUser.save();
+    res.status(201).json({
+        _id:NewUser._id,
+        fullName : NewUser.fullName,
+        email:NewUser.email,
+        PFP:NewUser.PFP
+    });
+}else{ 
+    res.status(400).json({message:"invalid user data"})
 }
-export const login = (req,res)=> {
-    res.send("login route")
+
+
+} catch (error) {
+    console.log("ERROR : ",error.message)
 }
-export const logout = (req,res)=> {
-    res.send("logout route")
-}
+};
+export const login = (req, res) => {
+  res.send("login route");
+};
+export const logout = (req, res) => {
+  res.send("logout route");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,15 +3,25 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
 
+//store server in a variable to perform operations
 const app = express();
 
+//middleware used to grab data for auth.controller
+app.use(express.json())
+
+//setting up PORT from .env
 dotenv.config()
 const PORT = process.env.PORT
 
+
+//listening server on PORT stored in .env
 app.listen(PORT,()=>{
     console.log("Server is running at  "+PORT)
 })
 
+
+//Using AuthAPI route
 app.use("/api/auth",authRoutes)
 
+//Calling connectDb function from lib/db.js
 connectDB()
