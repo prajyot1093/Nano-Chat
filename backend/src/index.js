57250bb8b221bd2a145d981b4026e12118d28598
PRAJYOT PUNDE <prajyot1093@gmail.com>
2026-07-28 00:15:32 +0530

    Updated : index.js and imported cookie parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,27 +1,30 @@
-import express from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
 
 //store server in a variable to perform operations
 const app = express();
 
 //middleware used to grab data for auth.controller
-app.use(express.json())
+app.use(express.json());
+
+//cookie parser middleware
+app.use(cookieParser());
 
 //setting up PORT from .env
-dotenv.config()
-const PORT = process.env.PORT
-
-
-//listening server on PORT stored in .env
-app.listen(PORT,()=>{
-    console.log("Server is running at  "+PORT)
-})
-
+const PORT = process.env.PORT || 5001;
 
 //Using AuthAPI route
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 
 //Calling connectDb function from lib/db.js
-connectDB()
+connectDB();
+
+//listening server on PORT stored in .env
+app.listen(PORT, () => {
+  console.log("Server is running at " + PORT);
+});
